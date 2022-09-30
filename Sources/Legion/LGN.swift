3b97a26f2b09064b780f5afd84e47dc66969d21c
isa nur fajar <ishafajar11@gmail.com>
2022-09-30 22:07:10 +0700

    fix

--- Sources/Legion/LGN.swift
+++ Sources/Legion/LGN.swift
@@ -7,54 +7,32 @@
 
 import Foundation
 
-//public class LGN {
-//    public static var sharedInstance : LGN!
-//    public var config : Ecosystem?
-//
-//    public init(config: Ecosystem?) {
-//        self.config = config
-//        LGN.sharedInstance = self
-//    }
-//
-//    open class func shared(theme: Ecosystem?) -> LGN {
-//        switch sharedInstance {
-//        case let i?:
-//            i.config = theme
-//            return i
-//        default:
-//            sharedInstance = LGN(config: theme)
-//            return sharedInstance
-//        }
-//    }
-//}
-
-
-
 public class LGN {
-  public static var shared: LGN {
-    if let initializedShared = _shared {
-      return initializedShared
+    private static var _shared: LGN?
+    private var config: Ecosystem
+    
+    public static var shared: LGN {
+        if let initializedShared = _shared {
+            return initializedShared
+        }
+        fatalError("Singleton not yet initialized. Run setup(withConfig:) first")
     }
-    fatalError("Singleton not yet initialized. Run setup(withConfig:) first")
-  }
-  private static var _shared: LGN? // This can only be set by the setup() func since it is private
-  private var config: Ecosystem // The configuration for the singleton. Could be a `String` if so requested
-
-  /// The ParameterSingleton setup func. Will initialize the singleton with the config. Without a config, `shared` will cause a `fatalError`
-  ///
-  /// - Parameter config: The config needed for initializing the singleton
-  open class func setup(withConfig config: Ecosystem) {
-    _shared = LGN(withConfig: config)
-  }
-
-
-  // Make the init private so this class can only be used be invoking the `setup(withConfig:)` func
-  private init(withConfig config: Ecosystem) {
-    self.config = config
-  }
-
-  /// The public func to do something
-  public func doSomething() -> String? {
-      return config.module 
-  }
+    
+    /// The ParameterSingleton setup func. Will initialize the singleton with the config. Without a config, `shared` will cause a `fatalError`
+    ///
+    /// - Parameter config: The config needed for initializing the singleton
+    open class func setup(withConfig config: Ecosystem) {
+        _shared = LGN(withConfig: config)
+    }
+    
+    
+    // Make the init private so this class can only be used be invoking the `setup(withConfig:)` func
+    private init(withConfig config: Ecosystem) {
+        self.config = config
+    }
+    
+    /// The public func to do something
+    public func doSomething() -> String? {
+        return config.module
+    }
 }
